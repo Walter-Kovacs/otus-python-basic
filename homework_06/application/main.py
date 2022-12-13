fe252d2132c4b35b1a3079b6a041088f944ef22e
Roman Baranov <roma.baranov@gmail.com>
2022-12-13 19:43:13 +0300

    Homework 06: modifications for docker-compose

--- homework_06/application/main.py
+++ homework_06/application/main.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
@@ -12,8 +14,8 @@ app = Flask(__name__)
 app.register_blueprint(authors_bp, url_prefix="/authors/")
 app.register_blueprint(books_bp, url_prefix="/books/")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://username:passwd!@localhost:5432/library"
-app.config["SECRET_KEY"] = "asdf"
+CONFIG_OBJECT = getenv("CONFIG", "DevelopmentConfig")
+app.config.from_object(f"config.{CONFIG_OBJECT}")
 
 CSRFProtect(app)
 db.init_app(app)
@@ -23,7 +25,3 @@ migrate = Migrate(app, db)
 @app.get("/")
 def home():
     return render_template("home.html")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
